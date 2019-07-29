6fb83d6c41c1486539a9c5ecbf78d801dd24a3a9
Nikita Gvozdkov <33928886+NeGvozd@users.noreply.github.com>
2019-07-29 23:15:15 +0300

    Update send.php

--- send.php
+++ send.php
@@ -4,9 +4,9 @@ if(isset($_POST['submit'])){
 $to = "ngvozdkov@yandex.ru";; // Здесь нужно написать e-mail, куда будут приходить письма
 $from = $_POST['email']; // this is the sender's Email address
 $first_name = $_POST['first_name'];
-$subject = "Форма отправки сообщений с сайта";
+$subject = "fomr";
 $subject2 = "Copy of your form submission";
-$message = $first_name . " оставил сообщение:" . "\n\n" . $_POST['message'];
+$message = $first_name . " send:" . "\n\n" . $_POST['message'];
 $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
 $headers = "From:" . $from;
@@ -14,8 +14,8 @@ $headers2 = "From:" . $to;
 
 mail($to,$subject,$message,$headers);
 // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
-echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
-echo "<br /><br /><a href='https://epicblog.net'>Вернуться на сайт.</a>";
+echo "sucsses " . $first_name . ", yea";
+echo "<br /><br /><a href='https://epicblog.net'>back.</a>";
 
 }
 
